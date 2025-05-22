408400f42621f26f25ebc8faa29ec84119d16bbb
ryantran2003dt <ryantran2003dt@gmail.com>
2025-05-22 23:57:01 +0700

    last time

--- sale-course-cms/src/main/java/com/salecoursecms/service/impl/CourseSessionServiceImpl.java
+++ sale-course-cms/src/main/java/com/salecoursecms/service/impl/CourseSessionServiceImpl.java
@@ -1,10 +1,11 @@
 package com.salecoursecms.service.impl;
 
+import com.salecoursecms.constant.MessageConst;
 import com.salecoursecms.dto.reponse.CourseSessionReponse;
 import com.salecoursecms.entity.first.CourseEntity;
 import com.salecoursecms.entity.first.CourseSessionEntity;
+import com.salecoursecms.exception.SystemErrorException;
 import com.salecoursecms.mapper.CourseSessionMapper;
-import com.salecoursecms.repository.first.CourseRepository;
 import com.salecoursecms.repository.first.CourseSessionRepository;
 import com.salecoursecms.service.CourseSessionService;
 import com.salecoursecms.utils.CourseScheduleUtils;
@@ -32,48 +33,55 @@ public class CourseSessionServiceImpl implements CourseSessionService {
 
     @Override
     public List<CourseSessionReponse> generatorCourseSession(CourseEntity courseEntity) {
-        LocalDate current = courseEntity.getStartDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        List<CourseSessionEntity> courseSessionList = new ArrayList<>();
+        try{
+            LocalDate current = courseEntity.getStartDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
 
-        String sessionType = courseEntity.getSessionType() == 1 ? "ONLINE" : "OFFLINE";
+            List<CourseSessionEntity> courseSessionList = new ArrayList<>();
 
-        List<String> days = CourseScheduleUtils.splitDays(courseEntity.getDaysOfWeek());
-        List<DayOfWeek> studyDays = days.stream()
-                .map(day -> DayOfWeek.valueOf(day.toUpperCase()))
-                .collect(Collectors.toList());
+            String sessionType = courseEntity.getSessionType() == 1 ? "ONLINE" : "OFFLINE";
 
-        Set<LocalDate> holidays = VietnamHolidayUtils.getVietnamHolidays(current.getYear());
+            List<String> days = CourseScheduleUtils.splitDays(courseEntity.getDaysOfWeek());
 
-        int sessionCount = 0;
-        while (sessionCount < courseEntity.getNumberOfSession()) {
-            if (!studyDays.contains(current.getDayOfWeek()) || holidays.contains(current)) {
+            List<DayOfWeek> studyDays = days.stream()
+                    .map(day -> DayOfWeek.valueOf(day.toUpperCase()))
+                    .collect(Collectors.toList());
+
+            Set<LocalDate> holidays = VietnamHolidayUtils.getVietnamHolidays(current.getYear());
+
+            int sessionCount = 0;
+            while (sessionCount < courseEntity.getNumberOfSession()) {
+                if (!studyDays.contains(current.getDayOfWeek()) || holidays.contains(current)) {
+                    current = current.plusDays(1);
+                    continue;
+                }
+
+                if (current.getMonthValue() == 1 && current.getDayOfMonth() == 1) {
+                    holidays = VietnamHolidayUtils.getVietnamHolidays(current.getYear());
+                }
+
+                CourseSessionEntity session = new CourseSessionEntity();
+                session.setSessionDate(java.sql.Date.valueOf(current));
+                session.setCourseId(courseEntity.getId());
+                session.setCreateDate(new Date());
+                session.setStatus(1);
+
+                session.setTitle("Session " + (sessionCount + 1));
+                session.setDescription("");
+                session.setLocation("");
+                session.setSessionType(sessionType);
+
+                courseSessionList.add(session);
+                sessionCount++;
+
                 current = current.plusDays(1);
-                continue;
             }
 
-            if (current.getMonthValue() == 1 && current.getDayOfMonth() == 1) {
-                holidays = VietnamHolidayUtils.getVietnamHolidays(current.getYear());
-            }
-
-            CourseSessionEntity session = new CourseSessionEntity();
-            session.setSessionDate(java.sql.Date.valueOf(current));
-            session.setCourseId(courseEntity.getId());
-            session.setCreateDate(new Date());
-            session.setStatus(1);
-
-            session.setTitle("Session " + (sessionCount + 1));
-            session.setDescription("");
-            session.setLocation("");
-            session.setSessionType(sessionType);
-
-            courseSessionList.add(session);
-            sessionCount++;
-
-            current = current.plusDays(1);
+            courseSessionRepository.saveAll(courseSessionList);
+            List<CourseSessionReponse> reponseList = courseSessionMapper.toReponseList(courseSessionList);
+            return reponseList;
+        }catch (Exception e){
+            log.error("[ERROR] "+e.getMessage());
+            throw new SystemErrorException(MessageConst.INTERNAL_SERVER_ERROR);
         }
-
-        courseSessionRepository.saveAll(courseSessionList);
-        List<CourseSessionReponse> reponseList = courseSessionMapper.toReponseList(courseSessionList);
-        return reponseList;
     }
 }
